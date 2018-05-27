e63c0b50575c11f51d4444c5373a2598086e1562
Sam Sun <373000335@qq.com>
2018-05-27 02:14:58 -0400

    eureka service

---
+++ service-registration-and-discovery/src/main/java/com/sunrui/serviceregistrationanddiscovery/ServiceRegistrationAndDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.sunrui.serviceregistrationanddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrationAndDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryApplication.class, args);
+	}
+}
